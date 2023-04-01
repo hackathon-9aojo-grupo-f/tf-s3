64d1a66ba204e67fb1e464df8522c23d3045507e
Carlos Dias <carlosmazzon@hotmail.com>
2023-04-01 12:56:07 -0300

    setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,25 @@
 provider "aws" {
-  region = var.region
+  region = "us-east-2"
 }
 
 resource "aws_s3_bucket" "bucket" {
-  bucket = var.bucket_name
-  acl    = "public-read"
+  bucket = "video-training-9aojo-grupo-f"
+}
 
-  website {
-    index_document = "index.html"
-    error_document = "404.html"
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_website_configuration" "bucket_website" {
+  bucket = aws_s3_bucket.bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
   }
 }
+
